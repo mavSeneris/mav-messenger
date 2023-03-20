da8e93e28765982d814ca3afbe3efe8f9596ba9d
Maverick Seneris <mavseneris@gmail.com>
2023-03-20 16:44:45 +0800

    added chats component for sidebar

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,8 +2,63 @@ import React from 'react'
 
 export default function Chats() {
   return (
-    <div>
-      
+    <div className='chats'>
+      <div className='user-chat'>
+        <img src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdvbWVuJTIwcG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60' />
+        <div className="user-chat-info-wrapper">
+          <span className='user-chat-info'>Ily Seneris</span>
+          <p>All I know is Roblox!</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src='http://cdn.shopify.com/s/files/1/0745/0975/articles/gigachad_1024x1024.jpg?v=1667928905' />
+        <div className="user-chat-info-wrapper">
+          <span className='user-chat-info'>Mav Seneris</span>
+          <p>Hello I'm a developer!</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src='https://images.unsplash.com/photo-1558227108-83a15ddbbb15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHdvbWVuJTIwcG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60' />
+        <div className="user-chat-info-wrapper">
+          <span className='user-chat-info'>Jho Seneris</span>
+          <p>Nasan na si daddy mo?</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src='https://images.unsplash.com/photo-1617457204918-8f13db5b823e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGJlbGdpYW4lMjBtYWxpbmlvc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60' />
+        <div className="user-chat-info-wrapper">
+          <span className='user-chat-info'>Sam the dog</span>
+          <p>Arf! Arf! Cat!!!</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src='https://images.unsplash.com/photo-1578678890094-3ddb1146ca99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVhZ2xlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60' />
+        <div className="user-chat-info-wrapper">
+          <span className='user-chat-info'>Lily the dog</span>
+          <p>God make me lose my mind up in here up here!</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src='https://images.unsplash.com/photo-1597419701820-5f4ab36c3d7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnJpdHRpc2glMjBzaG9ydGhhaXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60' />
+        <div className="user-chat-info-wrapper">
+          <span className='user-chat-info'>Mollie the cat</span>
+          <p>Meow meow!</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src='https://images.unsplash.com/photo-1630126798948-1abb357a1ec5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc2lhbiUyMGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60' />
+        <div className="user-chat-info-wrapper">
+          <span className='user-chat-info'>Waffles the cat</span>
+          <p>Purr purr!</p>
+        </div>
+      </div>
+
     </div>
   )
 }
