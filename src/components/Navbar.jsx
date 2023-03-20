0bd5cd9db2b46e24c04aaa288dad2207d5c63b4f
Maverick Seneris <mavseneris@gmail.com>
2023-03-20 16:05:52 +0800

    added search component to sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <span className='navbar-logo'>Mav Messenger</span> 
       <div className='user'>
         <img className='user-nav-avatar' src='https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1lbiUyMHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'/>
-        <span className='user-name'>John Appleseed</span>
+        <span className='user-name'>Eliud Seneris</span>
         <button className='user-logout-btn'>Logout</button>
       </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export default function Search() {
+  const err = false
+  const user = false
+
   return (
-    <div>
-      
+    <div className='search'>
+      <div className='search-form'>
+        <input type='text' placeholder='Find user...' />
+      </div>
+      {err && <div className='search-error'>User not found! :'(</div>}
+      {user && <div className='user-chat'>
+        <img src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdvbWVuJTIwcG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60' />
+        <span className='user-chat-info'>Ily Seneris</span>
+      </div>}
     </div>
   )
 }
