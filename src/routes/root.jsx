c802434fa0902e4017c34d6617a4b5c77d610de4
Maverick Seneris <mavseneris@gmail.com>
2023-03-21 13:55:04 +0800

    Building app routes

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import Home from '../pages/Home'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
 
 export default function Root() {
   return (
     <div>
-      {/* <Login/> */}
-      {/* <Register/> */}
-      {/* <Home/> */}
+      
     </div>
   )
 }
